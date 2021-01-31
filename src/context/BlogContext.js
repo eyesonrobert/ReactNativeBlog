3be228e066c390907b4969f01893a5876128015e
Robert Franklin <eyesonrobert@gmail.com>
2021-01-31 08:04:11 -0800

    Add and delete created

---
+++ src/context/BlogContext.js
@@ -0,0 +1,39 @@
+/** @format */
+
+import createDataContext from './createDataContext';
+
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case 'delete_blog_post':
+      return state.filter(blogPost => blogPost.id !== action.payload);
+      
+    case 'add_blog_post':
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 999999999),
+          title: `Blog Post # ${state.length + 1}`,
+        },
+      ];
+    default:
+      return state;
+  }
+};
+
+const addBlogPost = (dispatch) => {
+  return () => {
+    dispatch({ type: 'add_blog_post' });
+  };
+};
+
+const deleteBlogPost = (dispatch) => {
+  return (id) => {
+    dispatch({ type: 'delete_blog_post', payload: id });
+  };
+};
+
+export const { Context, Provider } = createDataContext(
+  blogReducer,
+  { addBlogPost, deleteBlogPost },
+  []
+);
---
+++ src/context/createDataContext.js
@@ -0,0 +1,22 @@
+/** @format */
+
+import React, { useReducer } from 'react';
+
+export default (reducer, actions, initialState) => {
+  const Context = React.createContext();
+
+  const Provider = ({ children }) => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    const boundActions = {};
+
+    for(let key in actions){
+        //key === 'add_blog_post'
+        boundActions[key] = actions[key](dispatch)
+    }
+
+    return <Context.Provider value={{ state, ...boundActions }}>{children}</Context.Provider>;
+  };
+
+  return { Context, Provider };
+};
